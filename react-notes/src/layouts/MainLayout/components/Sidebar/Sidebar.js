03f0ae619beb3c5dfcf1675209c0c42a1efd3fe9
DANIDIA <danik.serduk@gmail.com>
2023-11-06 21:49:38 +0100

    feat(Sidebar): text 'Articles:' change of "No articles" when there aren't articles in sidebar

--- react-notes/src/layouts/MainLayout/components/Sidebar/Sidebar.js
+++ react-notes/src/layouts/MainLayout/components/Sidebar/Sidebar.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import styles from './Sidebar.module.css';
 import { ArticleLink } from './components';
 import { ArticlesContext } from '../../../../pages/article/context';
+import classNames from 'classnames';
 
 export const Sidebar = (props) => {
     const { articles } = useContext(ArticlesContext);
@@ -14,7 +15,13 @@ export const Sidebar = (props) => {
                 Add article
             </Link>
 
-            <h3 className={styles.title}>Articles: </h3>
+            <h3
+                className={classNames(styles.title, {
+                    [styles.noArticles]: !articles.length,
+                })}
+            >
+                {articles.length ? 'Articles:' : 'No articles'}
+            </h3>
 
             <ul>
                 {articles.map((article) => (
