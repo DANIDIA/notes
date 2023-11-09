2ff076df40b83f85ab5ca32c935a6f44f8156a42
DANIDIA <danik.serduk@gmail.com>
2023-11-09 16:40:04 +0100

    feat(MainLayout): make sidebar responsive

--- react-notes/src/layouts/MainLayout/MainLayout.js
+++ react-notes/src/layouts/MainLayout/MainLayout.js
@@ -2,16 +2,28 @@ import { Outlet } from 'react-router-dom';
 
 import { Header, Sidebar } from './components';
 import styles from './MainLayout.module.css';
+import { useEffect, useState } from 'react';
 
 export const MainLayout = () => {
-    const height = document.documentElement.clientHeight;
+    const [windowHeight, setWindowHeight] = useState(window.innerHeight);
+
+    useEffect(() => {
+        const onWindowHeightResizeHandle = () => {
+            setWindowHeight(window.innerHeight);
+        };
+
+        window.addEventListener('resize', onWindowHeightResizeHandle);
+
+        return () =>
+            window.removeEventListener('resize', onWindowHeightResizeHandle);
+    }, []);
 
     return (
-        <div style={{ minHeight: height }}>
+        <div style={{ minHeight: windowHeight }}>
             <Header />
 
             <div className={styles.content}>
-                <Sidebar style={{ height: height }} />
+                <Sidebar style={{ height: windowHeight }} />
 
                 <div className={styles.articleContainer}>
                     <Outlet />
